c4b8a8c8113d8788f06d536252148050140c60c9
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-05-29 14:05:21 +0300

    Update Entity.java

--- src/com/pacman/Entity.java
+++ src/com/pacman/Entity.java
@@ -1,18 +1,10 @@
 package com.pacman;
 
-
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
 
-public abstract class Entity {
-    protected int x;
-    protected int y;
-    protected AffineTransform transform;
-    protected BufferedImage sprite;
-
-    public abstract void draw(Graphics g);
-    public abstract void update(double delta, GameMap map);
-    public abstract void handle(KeyEvent e, GameMap map);
+public interface Entity {
+    void draw(Graphics g);
+    void update(double delta, Game game);
+    void handle(KeyEvent e, Game game);
 }
