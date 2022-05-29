ad1b7915cff8cb7b18afd70e6473a6ded11ba909
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-05-29 14:05:55 +0300

    Update GamePanel.java

--- src/com/pacman/GamePanel.java
+++ src/com/pacman/GamePanel.java
@@ -1,26 +1,19 @@
 package com.pacman;
 
-import org.lwjgl.system.NonnullDefault;
-
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class GamePanel extends JFrame {
 
     public GamePanel(String title, Game game) {
         super(title);
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-
         this.add(game);
-        this.setLocationRelativeTo(null);
         this.pack();
         this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.toFront();
+        this.requestFocus();
     }
 
 }
