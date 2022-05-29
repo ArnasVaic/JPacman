abe51a13c4f165aaa24ba3b9d2a098ce08458d24
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-05-29 14:05:51 +0300

    Update GameMap.java

--- src/com/pacman/GameMap.java
+++ src/com/pacman/GameMap.java
@@ -1,80 +1,123 @@
 package com.pacman;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class GameMap {
 
-    public final static int tileCountX = 28;
-    public final static int tileCountY = 36;
-    public final static int tileWidth = 8;
-    public final static int tileHeight = 8;
+    public static final Point TILE_COUNT = new Point(28, 36);
+    public static final Point SCALED_TILE_COUNT = new Point(TILE_COUNT.x * Game.SCALE, TILE_COUNT.y * Game.SCALE);
+    public static final Point TILE_SIZE = new Point(8, 8);
+    public static final Point SCALED_TILE_SIZE = new Point(TILE_SIZE.x * Game.SCALE, TILE_SIZE.y * Game.SCALE);
 
-    private BufferedImage image;
-    private AffineTransform transform;
+    public static final Point MIN_BOUNDS = new Point(0, 0);
+    public static final Point MAX_BOUNDS = PointUtils.sub(TILE_COUNT, new Point(1, 1));
 
-    public boolean renderGrid = false;
 
-    // Each bit in this string is a boolean value which determines
-    // if a tile is solid or not. Hex character index hold the information
-    // about tile position
-    private boolean[] collisionData;
+    public static final int TURN            = 1;
+    public static final int INTERSECTION    = 0;
+    public static final int BIG_FOOD        = 1;
+    public static final int SMALL_FOOD      = 0;
+
+    Set<Point> junctions; // junction positions
+    Map<Point, Integer> food; // food (true: large, false: small)
+
+    public boolean renderGrid = true;
+
+    private boolean[] solid;
 
     public GameMap() {
-        transform = new AffineTransform();
-        transform.setToScale(Game.SCALE, Game.SCALE);
-        try {
-            image = ImageIO.read(new File("assets/map.png"));
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
+        parseMetaData(Assets.metadata);
+    }
 
-        // read map collision data and convert it to binary
-        collisionData = new boolean[tileCountX * tileCountY];
-        byte[] bytes = {0};
-        try {
-            bytes = Files.readAllBytes(Paths.get("assets/mapdata"));
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-        final int bitsPerByte = 8;
-        for(int i = 0; i < bytes.length; i++) {
-            for(int j = 0; j < bitsPerByte; j++) {
-                final int index = bitsPerByte * i + bitsPerByte - 1 - j;
-                final int flag = (bytes[i] >> j) & 1;
-                collisionData[index] = (flag == 1);
+    public void parseMetaData(BufferedImage metaImage) {
+        assert(metaImage.getWidth() == GameMap.TILE_COUNT.x);
+        assert(metaImage.getHeight() == GameMap.TILE_COUNT.y);
+
+        food = new HashMap<>();
+        junctions = new HashSet<>();
+        solid = new boolean[TILE_COUNT.x * TILE_COUNT.y];
+
+        for(int i = 0; i < metaImage.getHeight(); i++) {
+            for(int j = 0; j < metaImage.getWidth(); j++) {
+                // current pixel color
+                final Color color = new Color(metaImage.getRGB(j, i));
+                // convert coordinates to 1d index
+                final int index = j + i * metaImage.getWidth();
+                // current pixel coordinates
+                final Point p = new Point(j, i);
+                // solid tile value: 255, empty tile value: 0
+                final int red = color.getRed();     // red channel      - collision data
+                // small food value: 255, big food value: 127
+                final int green = color.getGreen(); // green channel    - food data
+                // junction value: 255
+                final int blue = color.getBlue();   // blue channel     - junction data
+                // set tile solidity
+                solid[index] = (red == 255);
+                if(blue == 255) junctions.add(p);
+                switch(green) {
+                    case 255 -> food.put(p, SMALL_FOOD);
+                    case 127 -> food.put(p, BIG_FOOD);
+                }
             }
         }
     }
 
     public void draw(Graphics g) {
-        Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(image, transform, null);
+        final int x = TILE_SIZE.x * Game.SCALE;
+        final int y = TILE_SIZE.y * Game.SCALE;
+        final int w = Game.SIZE.x * Game.SCALE;
+        final int h = Game.SIZE.y * Game.SCALE;
+
+        g.drawImage(Assets.map, 0, 0, Game.SIZE.x, Game.SIZE.y, null);
+
+        for (Map.Entry<Point, Integer> entry : food.entrySet()) {
+            Point p = entry.getKey();
+            Integer index = entry.getValue();
+            g.drawImage(Assets.food[index], p.x * x, p.y * y, x, y, null);
+        }
 
         if(renderGrid) {
-            g.setColor(new Color(255, 255, 255, 127));
-            for(int i = 0; i < tileCountX; i++) {
-                final int x = tileWidth * Game.SCALE;
-                final int w = Game.WIDTH * Game.SCALE;
-                g.drawLine(i * x, 0, i * x, w);
+            // Paint grid
+            g.setColor(new Color(255, 255, 255, 40));
+            for(int i = 0; i < TILE_COUNT.x; ++i) {
+                g.fillRect(i * x - 1, 0, 2, h);
             }
-            for(int i = 0; i < tileCountY; i++) {
-                final int y = tileHeight * Game.SCALE;
-                final int h = Game.HEIGHT * Game.SCALE;
-                g.drawLine(0, i * y, h, i * y);
+            for(int i = 0; i < TILE_COUNT.y; ++i) {
+                g.fillRect(0, i * y - 1, w, 2);
+            }
+            // Paint turns & intersections
+            g.setColor(new Color(0, 255, 0, 40));
+            for (Point p : junctions) {
+                g.fillRect(p.x * x, p.y * y, x, y);
             }
         }
     }
 
     public boolean isTileSolid(int x, int y) {
-        return collisionData[x + y * tileCountX];
+        final int index = x + y * TILE_COUNT.x;
+        return solid[index];
+    }
+
+    public boolean isTileSolid(Point p) {
+        final int index = p.x + p.y * TILE_COUNT.x;
+        return solid[index];
+    }
+
+    public boolean isJunction(Point point) {
+        return junctions.contains(point);
+    }
+
+    public boolean isFood(Point point, int type) {
+        if(!food.containsKey(point)) return false;
+        return (food.get(point) == type);
+    }
+
+    public void removeFood(Point point) {
+        food.remove(point);
     }
 }
